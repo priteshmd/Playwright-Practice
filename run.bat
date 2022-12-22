efbd8d82ba1d96472b7da0cd4ac6461dbfaf2af6
PRITESH <priteshmd@gmail.com>
2022-12-23 01:42:48 +0530

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
 call npm install
-mkdir ".\node_modules\puppeteer-core\.local-chromium\chromium-1041"
-move ".\node_modules\puppeteer-core\.local-chromium\win64-1045629\chrome-win" ".\node_modules\puppeteer-core\.local-chromium\chromium-1041"
+mkdir ".\node_modules\puppeteer-core\.local-chromium\chromium-1028"
+move ".\node_modules\puppeteer-core\.local-chromium\win64-1045629\chrome-win" ".\node_modules\puppeteer-core\.local-chromium\chromium-1028"
 SET PLAYWRIGHT_BROWSERS_PATH=.\node_modules\puppeteer-core\.local-chromium\
 echo "set path Done %cd%"
 call npm run test:chromium
